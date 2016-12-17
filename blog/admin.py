502bffc9b22fbfe9f7deda5d6e4c7a4c43691b1f
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-18 02:25:44 +0530

    Admin changes.

--- blog/admin.py
+++ blog/admin.py
@@ -2,16 +2,28 @@ from django.contrib import admin
 
 from .models import Blog, Author, Entry, Tag
 
-admin.site.register(Author)
-admin.site.register(Blog)
-admin.site.register(Tag)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('headline', 'pub_date', 'mod_date',)
-    list_filter = ('pub_date', 'authors', 'rating')
-    fields = (('blog', 'headline'), 'body_text', 'authors',
+    list_filter = ('pub_date', 'rating')
+    fields = (('blog', 'headline'), 'body_text', ('authors', 'tags'),
               ('pub_date', 'mod_date'),
               ('n_comments', 'n_pingbacks', 'rating'))
     date_hierarchy = 'pub_date'
