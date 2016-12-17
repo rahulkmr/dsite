f351d85a00ee45d7264b2428e63b5764c42a5114
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-18 01:58:17 +0530

    admin changes.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, Author, Entry
+
+admin.site.register(Author)
+admin.site.register(Blog)
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'pub_date', 'mod_date',)
+    list_filter = ('pub_date', 'authors', 'rating')
+    fields = (('blog', 'headline'), 'body_text', 'authors', ('pub_date', 'mod_date'),
+              ('n_comments', 'n_pingbacks', 'rating'))
+    date_hierarchy = 'pub_date'
+    search_fields = ('headline', 'authors__name', 'authors__email')
