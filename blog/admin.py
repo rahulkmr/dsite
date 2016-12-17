27569f0b9748bf981dcd3aad8eeaab884de56d59
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-18 02:12:32 +0530

    admin tags.

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 
-from .models import Blog, Author, Entry
+from .models import Blog, Author, Entry, Tag
 
 admin.site.register(Author)
 admin.site.register(Blog)
+admin.site.register(Tag)
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('headline', 'pub_date', 'mod_date',)
     list_filter = ('pub_date', 'authors', 'rating')
-    fields = (('blog', 'headline'), 'body_text', 'authors', ('pub_date', 'mod_date'),
+    fields = (('blog', 'headline'), 'body_text', 'authors',
+              ('pub_date', 'mod_date'),
               ('n_comments', 'n_pingbacks', 'rating'))
     date_hierarchy = 'pub_date'
     search_fields = ('headline', 'authors__name', 'authors__email')
