0d9e0c68af8a38ce140bff986d21b7342c06b03f
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-18 02:00:40 +0530

    Tag model.

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,13 @@ class Author(models.Model):
 
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Entry(models.Model):
     blog = models.ForeignKey(Blog)
     headline = models.CharField(max_length=255)
