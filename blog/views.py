1c4469c405c0edce4bc7d8bf9b98ad34e4335445
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-21 00:09:27 +0530

    Detail view.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Entry
 
 
 class EntryListView(ListView):
+    model = Entry
+    paginate_by = Entry.PAGE_LIMIT
+
+
+
+class EntryDetailView(DetailView):
     model = Entry
