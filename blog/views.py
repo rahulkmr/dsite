b2862258e24dc5eedc0e823a451efa3981c1da76
Rahul Kumar <rahul@thoughtnirvana.com>
2016-12-21 17:40:22 +0530

    Comment add view.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
-from .models import Entry
+from .models import Entry, Comment
+from .forms import CommentForm
 
 
 class EntryListView(ListView):
@@ -13,3 +15,13 @@ class EntryListView(ListView):
 
 class EntryDetailView(DetailView):
     model = Entry
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm(initial={'entry': context['object'].id})
+        return context
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    fields = ['commenter', 'comment', 'entry']
